e67e00a99b4015f5ac1448680434fe2f235d498f
Admin <Admin@DESKTOP-2P6A2VA.lan>
2025-04-22 08:34:39 -0700

    This is my first commit

--- Java222025/src/practice/LogicalPrograms.java
+++ Java222025/src/practice/LogicalPrograms.java
@@ -1,5 +1,24 @@
 package practice;
 
 public class LogicalPrograms {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int a=10,b=20;
+			/*
+			 * int temp; temp=a; a=b; b=temp;
+			 */
+			
+			a=a+b;
+			b=a-b;
+			a=a-b;
+			System.out.println(a+"\n"+b);
+			
+			
 
-}
+		}
+
+	}
+
+

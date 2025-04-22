a1ba1386211678a74ed2081584bd003aed94d0e6
Admin <Admin@DESKTOP-2P6A2VA.lan>
2025-04-22 08:44:02 -0700

    This Is My First Commit

--- Java222025/src/practice/LogicalPrograms.java
+++ Java222025/src/practice/LogicalPrograms.java
@@ -5,16 +5,14 @@ public class LogicalPrograms {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			
-			int a=10,b=20;
+			//int a=10,b=20;
 			/*
 			 * int temp; temp=a; a=b; b=temp;
 			 */
 			
-			a=a+b;
-			b=a-b;
-			a=a-b;
-			System.out.println(a+"\n"+b);
-			
+			/*
+			 * a=a+b; b=a-b; a=a-b; System.out.println(a+"\n"+b);
+			 */
 			
 
 		}
